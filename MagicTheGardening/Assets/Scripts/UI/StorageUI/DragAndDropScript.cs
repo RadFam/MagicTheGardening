1ee2b963ac462a6aa9d084de1b5221ffd5a9d6c2
RadFam <artkorkh@gmail.com>
2020-10-21 16:53:44 +0300

    Start work with storage UI

---
+++ MagicTheGardening/Assets/Scripts/UI/StorageUI/DragAndDropScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler , IDropHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+}
---
+++ MagicTheGardening/Assets/Scripts/UI/StorageUI/ExchangeUIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameElement;
+using GameControllers;
+
+public class ExchangeUIController : MonoBehaviour {
+
+    public Camera cam;
+
+    public PlayerStorageUIController playerPrefab;
+    public ChestStorageUIController chestPrefab;
+    public SalesmanStorageUIController salesPrefab;
+    public Button exitButton;
+
+    StorageScript SSc_obj;
+    StorageScript SSc_subj;
+    
+    // Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+
+    public void ShowPlayerChest(ref StorageScript ssc_1, ref StorageScript ssc_2)
+    {
+        SSc_obj = ssc_1;
+        SSc_subj = ssc_2;
+
+        // Instantiate storage panels by the placement of objects
+        Vector3 obj_pos = cam.WorldToScreenPoint(ssc_1.gameObject.transform.position);
+        Vector3 subj_pos = cam.WorldToScreenPoint(ssc_2.gameObject.transform.position);
+
+
+    }
+
+    public void ShowPlayerSales()
+    {
+
+    }
+}

c7cabe39f2798bb465ce0abdfa2e7448c5a67747
RadFam <artkorkh@gmail.com>
2021-01-13 22:36:45 +0300

    Trade place build 02

---
+++ MagicTheGardening/Assets/Scripts/UI/TradeUI/TradePanelUIController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameUI
+{
+    public class TradePanelUIController : MonoBehaviour
+    {
+		public Text volText;
+		public Text fullCostText;
+		public Text oneCostText;
+
+		public Slider mySlide;
+
+		int costOne;
+		int costFull;
+		int volFull;
+		int maxVol; // How much is in the 
+		int maxCost;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+		public void OnEnable()
+		{
+			costOne = 0;
+			costFull = 0;
+			volFull = 0;
+			maxVol = 0;
+			maxCost = 0;
+
+			ShowParams();
+		}
+
+		public void SetInitParams(int maxGold, int maxVol, int costGold)
+		{
+			costOne = costGold;
+			mySlide.minValue = 0;
+			mySlide.maxValue = Mathf.Min(maxVol, maxGold/costGold);
+		}
+		void ShowParams()
+		{
+			volFull = (int)mySlide.value;
+			volText.text = volFull.ToString();
+			oneCostText.text = costOne.ToString();
+			costFull = volFull * costOne;
+			fullCostText.text = costFull.ToString();
+		}
+
+		public void OnOkButtonClick()
+		{
+			
+		}
+    }
+}
